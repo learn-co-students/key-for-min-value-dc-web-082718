8c662e507f51db5804444ae07e1b670e7294ee7c
Trevor Jameson <tjameson15@gmail.com>
2018-07-21 15:22:05 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,22 +5,26 @@ def key_for_min_value(name_hash)
   if name_hash == nil || name_hash.size == 0
     return nil
   end
-  small_value = 0
-  winner = nil
 
-  name_hash.collect do |key, value|
-    if small_value == 0
-      small_value = value
-      winner = key
-    elsif small_value > value
-      small_value = value
-      winner = key
+  # Initialize smallest to test key and largest possible value
+  # Intialize saved_key to test key
+  # For each pair in hash
+  # If pair[value] is less than smallest[saved_key], smallest equals pair and saved_key equals pair[key]
+  # Return saved_key after loop execution
+
+  smallest = {:init => 1000000000000}
+  saved_key = :init
+
+  name_hash.each do |key, value|
+    if smallest == {}
+      smallest = {key => value}
+    elsif value < smallest[saved_key]
+      smallest = {key => value}
+      saved_key = key
     end
   end
-  return name_hash[winner]
+  saved_key
 end
 
-# Test data:
-data = {:blake => 500, :ashley => 2, :adam => 1}
-# Test call:
-key_for_min_value(data)
+# Test data: data = {:blake => 500, :ashley => 2, :adam => 1}
+# Test call: key_for_min_value(data)
